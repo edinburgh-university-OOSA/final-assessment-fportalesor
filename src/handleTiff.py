710c4ca439af5d52dbee51bbc42748ee52f1db62
fportalesor <fportales.or@gmail.com>
2025-03-26 11:56:44 +0000

    adding arguments to writeTiff function

--- src/handleTiff.py
+++ src/handleTiff.py
@@ -10,7 +10,7 @@ A class to handle geotiffs
 from pyproj import Proj, transform # package for reprojecting data
 from osgeo import gdal             # package for handling geotiff data
 from osgeo import osr              # package for handling projection information
-from gdal import Warp
+#from gdal import Warp
 import numpy as np
 
 
@@ -27,18 +27,38 @@ class tiffHandle():
     '''
     Class initialiser
     Does nothing as this is only an example
+
     '''
 
 
   ########################################
 
-  def writeTiff(self,data,filename="chm.tif",epsg=27700):
+  def writeTiff(self,data, x, y, res, filename="chm.tif",epsg=27700):
     '''
     Write a geotiff from a raster layer
     '''
+    # determine bounds
+    minX=np.min(x)
+    maxX=np.max(x)
+    minY=np.min(y)
+    maxY=np.max(y)
+
+    # determine image size
+    nX=int((maxX-minX)/res+1)
+    nY=int((maxY-minY)/res+1)
+
+    # pack in to array
+    imageArr=np.full((nY,nX),-999.0)
+
+    # calculate the raster pixel index in x and y
+    xInds=np.array(np.floor((x-np.min(x))/res),dtype=int)   # need to force to int type
+    yInds=np.array(np.floor((np.max(y)-y)/res),dtype=int)
+
+    # this is a simple pack which will assign a single footprint to each pixel
+    imageArr[yInds,xInds]=data
 
     # set geolocation information (note geotiffs count down from top edge in Y)
-    geotransform = (self.minX, self.res, 0, self.maxY, 0, -1*self.res)
+    geotransform = (minX, res, 0, maxY, 0, -1*res)
 
     # load data in to geotiff object
     dst_ds = gdal.GetDriverByName('GTiff').Create(filename, self.nX, self.nY, 1, gdal.GDT_Float32)
@@ -47,7 +67,7 @@ class tiffHandle():
     srs = osr.SpatialReference()            # establish encoding
     srs.ImportFromEPSG(epsg)                # WGS84 lat/long
     dst_ds.SetProjection(srs.ExportToWkt()) # export coords to file
-    dst_ds.GetRasterBand(1).WriteArray(data)  # write image to the raster
+    dst_ds.GetRasterBand(1).WriteArray(imageArr)  # write image to the raster
     dst_ds.GetRasterBand(1).SetNoDataValue(-999)  # set no data value
     dst_ds.FlushCache()                     # write to disk
     dst_ds = None
