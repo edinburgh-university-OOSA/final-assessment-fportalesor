ad61a618b95fdccae9815e4f97851258e338e5a1
fportalesor <fportales.or@gmail.com>
2025-03-30 21:13:30 +0100

    Add function to transform coordinates and average point values within pixels

--- src/handleTiff.py
+++ src/handleTiff.py
@@ -1,5 +1,3 @@
-
-
 '''
 A class to handle geotiffs
 '''
@@ -34,18 +32,40 @@ class tiffHandle(lvisGround):
 
     return x_new, y_new
 
-  def writeTiff(self, data, x, y, res, filename="chm.tif",epsg=3031):
+  def writeTiff(self, data, x, y, res, filename="chm.tif", in_epsg=4326, out_epsg=3031, reproject=True):
     '''
-    Write a geotiff from a raster layer
+      Writes a geotiff from raster data.
+
+        Args:
+            data (numpy.ndarray): Array of data values to write to raster
+            x (array-like): X-coordinates of data points
+            y (array-like): Y-coordinates of data points
+            res (float): Resolution of output raster in target units
+            filename (str, optional): Output filename. Defaults to "chm.tif".
+            in_epsg (int, optional): EPSG code of input coordinates. Defaults to 4326.
+            out_epsg (int, optional): EPSG code for output geotiff. Defaults to 3031.
+            reproject (bool, optional): Whether to reproject coordinates. Defaults to True.
+
+        Returns:
+            None: Writes file to disk
+
+        Note:
+            When reproject=False, the output EPSG will match the input EPSG.
+            NoData values are set to -999.
     '''
-    # Reproject the coordinates from EPSG:4326 (lat/lon) to EPSG:3031 (Antarctic Polar Stereographic)
-    x_reprojected, y_reprojected = self.reprojectCoordinates(x, y, in_epsg=4326, out_epsg=epsg)
+    if reproject:
+      # Reproject the coordinates from input EPSG to target EPSG
+      x_processed, y_processed = self.reprojectCoordinates(x, y, in_epsg=in_epsg, out_epsg=out_epsg)
+    else:
+      # Use coordinates as-is
+      x_processed, y_processed = x, y
+      out_epsg = in_epsg  # Use input EPSG as output EPSG when not reprojecting
     
     # determine bounds
-    minX=np.nanmin(x_reprojected) #nanmin and nanmax to avoid nans
-    maxX=np.nanmax(x_reprojected)
-    minY=np.nanmin(y_reprojected)
-    maxY=np.nanmax(y_reprojected)
+    minX=np.min(x_processed)
+    maxX=np.max(x_processed)
+    minY=np.min(y_processed)
+    maxY=np.max(y_processed)
 
     # determine image size
     nX=int((maxX-minX)/res+1)
@@ -54,12 +74,18 @@ class tiffHandle(lvisGround):
     # pack in to array
     imageArr=np.full((nY,nX),-999.0)
 
-    # calculate the raster pixel index in x and y
-    xInds=np.array(np.floor((x_reprojected -np.min(x_reprojected))/res),dtype=int)   # need to force to int type
-    yInds=np.array(np.floor((np.max(y_reprojected)-y_reprojected)/res),dtype=int)
+    # Loop through the LiDAR data points
+    for i in range(len(x_processed)):
+        x_ind = int(np.floor((x_processed[i] - minX) / res))
+        y_ind = int(np.floor((maxY - y_processed[i]) / res))  # Invert y-axis for GeoTIFF convention
 
-    # this is a simple pack which will assign a single footprint to each pixel
-    imageArr[yInds,xInds]=data
+        # Ensure the indices are within the bounds of the raster
+        if 0 <= x_ind < nX and 0 <= y_ind < nY:
+            if imageArr[y_ind, x_ind] == -999.0:
+                imageArr[y_ind, x_ind] = data[i]  # First data point for this pixel
+            else:
+                # Average the values (add the new value and divide by the number of points)
+                imageArr[y_ind, x_ind] = (imageArr[y_ind, x_ind] + data[i]) / 2
 
     # set geolocation information (note geotiffs count down from top edge in Y)
     geotransform = (minX, res, 0, maxY, 0, -1*res)
@@ -69,7 +95,7 @@ class tiffHandle(lvisGround):
     dst_ds.SetGeoTransform(geotransform)    # specify coords
 
     srs = osr.SpatialReference()            # establish encoding
-    srs.ImportFromEPSG(epsg)                # WGS84 lat/long
+    srs.ImportFromEPSG(out_epsg)           # Use the output EPSG code
     dst_ds.SetProjection(srs.ExportToWkt()) # export coords to file
 
     dst_ds.GetRasterBand(1).WriteArray(imageArr)  # write image to the raster
@@ -82,7 +108,7 @@ class tiffHandle(lvisGround):
 
   ########################################
 
-def readTiff(self,filename):
+  def readTiff(self,filename):
     '''
     Read a geotiff in to RAM
     '''
@@ -102,7 +128,3 @@ def readTiff(self,filename):
     self.pixelHeight=transform_ds[5]   # resolution in y direction
     # read data. Returns as a 2D numpy array
     self.data=ds.GetRasterBand(1).ReadAsArray(0,0,self.nX,self.nY)
-
-
-#######################################################
-
