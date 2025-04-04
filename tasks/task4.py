322a0ffcfbcd6b3782ac2d14920929b00c932ea1
fportalesor <fportales.or@gmail.com>
2025-04-04 15:13:15 +0100

    add script for task 4

---
+++ tasks/task4.py
@@ -0,0 +1,44 @@
+from _bootstrap import *
+import argparse
+import glob
+import os
+from src.sectionProcessing import LVISSectionProcessor
+from src.handleTiff import tiffHandle
+from src.plotting import plotLVIS
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process LVIS data to generate DEMs.")
+
+    parser.add_argument('-y', '--year', type=str, required=True,
+                      help="Year of data collection (e.g., 2009, 2015)")
+    parser.add_argument('-md', '--max_distance', type=int, default=None,
+                      help="Maximum search distance for gap filling (pixels)")
+    parser.add_argument('-s', '--smoothing', type=int, default=None,
+                      help="Smoothing iterations for gap filling")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    # Load tiff Handler
+    filename = '/geos/netdata/oosa/assignment/lvis/2009/ILVIS1B_AQ2009_1020_R1408_058456.h5'
+    tiffHandler = tiffHandle(filename)
+    
+    # Create combined mosaics and apply gap-filler algorithm
+    print(f"\nProcessing year: {args.year}")
+    success = tiffHandler.create_combined_mosaic(year=args.year,
+                                                create_filled=True,
+                                                max_distance=args.max_distance,
+                                                smoothing=args.smoothing)
+        
+    if success:
+        print(f"Successfully processed year {args.year}")
+    
+    else:
+        print(f"Failed to process year {args.year}")
+
+    # Plot filled mosaics
+    tiff_path = os.path.join('processed_data', f'filled_mosaic_{args.year}.tif')
+    print(tiff_path)
+    plotter = plotLVIS(filename)
+    plotter.plotDEM(tiff_path)
